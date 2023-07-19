8b509f9e241296f154acf0b20d8531248021a938
Pakamas111 <6310210267@psu.ac.th>
2023-07-19 12:53:17 +0700

    Lab2 CRUD

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,12 +3,16 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 @RestController
 public class EmployeeController {
@@ -32,6 +36,47 @@ public class EmployeeController {
 		data.add(body);
 		return body;
 
+	}
+
+	@GetMapping("/employee/{employeeId}")
+	public Employee getEmployeeDetail(@PathVariable Integer employeeId) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				return data.get(i);
+			}
+		}
+		return null;
+	}
+
+	@PutMapping("/employee/{employeeId}")
+	public Employee updaEmployee(@PathVariable Integer employeeId, @RequestBody Employee body) {
+
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+
+				data.get(i).setFirstName(body.getFirstName());
+				data.get(i).setLastName(body.getLastName());
+				data.get(i).setSalary(body.getSalary());
+
+				return data.get(i);
+
+			}
+		}
+		return null;
+	}
+
+	@DeleteMapping("/employee/{employeeId}")
+	public String deleEmployee(@PathVariable Integer employeeId) {
+		for (int i = 0; i < data.size(); i++) {
+			if (employeeId == data.get(i).getEmployeeId()) {
+				data.remove(i);
+				
+				return "delete sucess";
+			}
+		}
+
+		return "emplyee not found";
 	}
 
 }
--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,8 @@
 package com.example.demo.model;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class Employee {
 	
 	private Integer employeeId;
